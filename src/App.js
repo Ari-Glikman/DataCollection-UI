26ad3c00ada6be881a258efe2533615a1d2dba1d
Ari-Glikman <73805987+Ari-Glikman@users.noreply.github.com>
2024-04-25 11:35:38 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import './App.css';
 import DBChart from "./components/DBChart.js"
-import DropDown from './components/DropDownGlobals';
 import BarChart from './components/BarChart';
+import SearchBar from './components/SearchBar';
 import React from "react";
 import TableInputBar from './components/TableInputBar';
 
 function App() {
 
-
   return (
 
     <div className="float-parent-element">
-     
-      {/* <canvas id = "DBChart"></canvas> */}
       <div className = "float-child-element1">
         
-        
-        
-        
       <h1>Database History</h1>
-       
       
       <br /> 
       <br /> 
@@ -30,15 +23,15 @@ function App() {
       <div className = "float-child-element1">
       <h2>Enter a Global To Inspect It</h2>
       
-      <DropDown/>
+      <div>
+        <SearchBar/>
+      </div>
       </div>
       {/* <br /> */}
       {/* <br /> */}
       <div className = "float-child-element1">
       <h1>Global Sizes   
-       </h1>
-      
-       
+       </h1>  
       <BarChart/>
       </div>
 
@@ -46,12 +39,7 @@ function App() {
       <h2>Tabled Data</h2>
 
       <TableInputBar/>
-
-
       </div>
-
-      
-  
     </div>
    
   );
